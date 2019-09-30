0e1ca6906890fae3b2788b9e7621173749813445
ajreyna <ajreyna88@gmail.com>
2019-09-30 09:52:52 -0500

    fixed weird problems

--- CS321_W2D1_BlogAPI/Services/PostService.cs
+++ CS321_W2D1_BlogAPI/Services/PostService.cs
@@ -40,23 +40,31 @@ namespace CS321_W2D1_BlogAPI.Services
         {
             // return the specified Post or null if not found
             // TODO: use FirstOrDefault() to find the Post by id in _posts and return it
+            // var currentPost;
+            return _posts.FirstOrDefault(p => p.Id == id);
         }
 
         public IEnumerable<Post> GetAll()
         {
             // TODO: return the full list of posts
+            return _posts;
         }
 
         public Post Update(Post updatedPost)
         {
             // get the Post object in the current list with this id 
             // TODO: find the post to update in the list, using updatedPost.Id, and assign to currentPost
+            var currentPost = this.Get(updatedPost.Id);
 
             // return null if the Post to update isn't found
             if (currentPost == null) return null;
 
+
             // copy the property values from the updated post into the current post object
             // TODO: copy the values in updatedPost to the post you found in the list
+            currentPost.Title = updatedPost.Title;
+            currentPost.Body = updatedPost.Body;
+
 
             return currentPost;
         }
@@ -64,6 +72,17 @@ namespace CS321_W2D1_BlogAPI.Services
         public void Remove(Post post)
         {
             // TODO: remove the post from _posts
+            var currentPost = _posts.FirstOrDefault(p => p.Id == post.Id);
+            if (currentPost != null)
+            {
+                _posts.Remove(post);
+            }
+            else
+            {
+                throw new Exception("Cannot remove nonexistent post");
+            }
+
+
         }
     }
 }
